9b0de188aca259d15193e2d1ff0d886dce9d12cc
matiasJoao <joaovictor.a4@gmail.com>
2023-10-03 21:34:04 -0300

    Mudando Arquivos

--- Back-end/src/repositories/UserRepo/index.ts
+++
@@ -1,5 +0,0 @@
-import { UserRopsitory } from "../../useCases/UserRepository";
-
-const userRopsitory = new UserRopsitory()
-
-export { userRopsitory }
--- Back-end/src/useCases/service/Impl/UserOwnerImpl.ts
+++
@@ -1,8 +0,0 @@
-import { UserOwnerDTO } from "../../DTO/UserOwnerDTO";
-import { UserOwnerInterface } from "../Interface/UserOwnerInterface";
-
-export class UserOwnerImpl implements UserOwnerInterface{
-    save(user: UserOwnerDTO): Promise<UserOwnerDTO> {
-        throw new Error("Method not implemented.");
-    }  
-}
